2b8fcaf2145ba73396899006958b4f7210b7c86b
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-02-28 00:18:58 +0100

    Update windspeed.py

--- windspeed.py
+++ windspeed.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
         print (get_value())
         angle = round(get_value(),1)
         print('angle ' + ' ' + str(angle))
-        print('rpm:{0:.2f}-RPM, nmh:{1:.3f}-knots, dist_meas:{2:.2f}m pulse:{3} elapse:{4:.3f}-start_timer:{5:.3f}'.format(rpm,nm_per_hour,dist_meas,pulse, elapse, start_timer))
+        print('rpm:{0:.2f}-RPM, nmh:{1:.3f}-knots, dist_meas:{2:.2f}m pulse:{3} elapse:{4:.3f}-start_timer:{5:.3f}'.format(rpm,nmh,dist_meas,pulse, elapse, start_timer))
         try:
             add_data(cursor,rpm, nmh, dist_meas, angle)
         except mariadb.Error as e:
