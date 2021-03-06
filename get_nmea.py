d4a2a6865d0d0f2bd14f27d80e1a11d72b3c5e35
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-03-06 22:38:22 +0100

    Update get_nmea.py

--- get_nmea.py
+++ get_nmea.py
@@ -26,15 +26,11 @@ list_of_valid_statuses = ['A','V']
 
 with serial.Serial('/dev/ttyAMA0', baudrate=4800, timeout=1) as ser:
     # read 10 lines from the serial output
-    for i in range(5):
+    for i in range(5): # print 5 lines to get rid of gibberich
         line = ser.readline().decode('ascii', errors='replace')
-#        print (line)
         decoded_line = (line.strip(string.punctuation))
-#        print (decoded_line)
-
     while True:
         line = ser.readline().decode('ascii', errors='replace')
-#        print (line)
         decoded_line = line.strip()
         if decoded_line[0:6] == '$GPVTG':
             print ("VTG line")
@@ -45,17 +41,18 @@ with serial.Serial('/dev/ttyAMA0', baudrate=4800, timeout=1) as ser:
         if decoded_line[0:6] == '$GPRMC':
             msg = pynmea2.parse(str(decoded_line))
             if str(msg.status) in list_of_valid_statuses:
-                print ("RMC line")
-                lat = ("%02d°%07.4f'" % (msg.latitude, msg.latitude_minutes))
-                lon = ("%02d°%07.4f'" % (msg.longitude, msg.longitude_minutes))
-                gps = "Latitude=" + str(lat) + "and Longitude=" + str(lon)
-                print(gps)
-                speed = msg.spd_over_grnd
-                print ('Speed over ground = ' + str(speed))
-                true_course = msg.true_course
-                print ('True Course = '+ str(true_course))
-                try:
-                    add_data(cursor,lat, lon, speed, true_course)
-                except mariadb.Error as e:
-                    print(f"Error inserting to db: {e}")
-                    sys.exit(1)
+               print(msg.status)
+               print ("RMC line")
+               lat = ("%02d°%07.4f'" % (msg.latitude, msg.latitude_minutes))
+               lon = ("%02d°%07.4f'" % (msg.longitude, msg.longitude_minutes))
+               gps = "Latitude=" + str(lat) + "and Longitude=" + str(lon)
+               print(gps)
+               speed = msg.spd_over_grnd
+               print ('Speed over ground = ' + str(speed))
+               true_course = msg.true_course
+               print ('True Course = '+ str(true_course))
+               try:
+                   add_data(cursor,lat, lon, speed, true_course)
+               except mariadb.Error as e:
+                   print(f"Error inserting to db: {e}")
+                   sys.exit(1)
