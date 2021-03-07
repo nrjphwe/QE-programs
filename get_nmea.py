89cb8dde75e80a785b456b337990126df84321be
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-03-07 23:02:25 +0100

    Update get_nmea.py

--- get_nmea.py
+++ get_nmea.py
@@ -54,9 +54,9 @@ with serial.Serial('/dev/ttyAMA0', baudrate=4800, timeout=1) as ser:
                 #print (msg.datetime,msg.latitude, msg.longitude)
                 #for i in range(len(msg.fields)):
                 #    print (msg.fields[i], msg.data[i])
-                lat = msg.lat
+                lat = msg.latitude
                 print (lat)
-                lon = msg.lon
+                lon = msg.longitude
                 #lat = ("%02d°%07.4f'" % (msg.latitude, msg.latitude_minutes))
                 #lon = ("%02d°%07.4f'" % (msg.longitude, msg.longitude_minutes))
                 #gps = "Latitude=" + str(lat) + "and Longitude=" + str(lon)
