d6955f1735fbf46889a7428c6abedf55591f012f
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-03-07 22:56:25 +0100

    Update get_nmea.py

--- get_nmea.py
+++ get_nmea.py
@@ -54,10 +54,12 @@ with serial.Serial('/dev/ttyAMA0', baudrate=4800, timeout=1) as ser:
                 #print (msg.datetime,msg.latitude, msg.longitude)
                 #for i in range(len(msg.fields)):
                 #    print (msg.fields[i], msg.data[i])
-                lat = ("%02d°%07.4f'" % (msg.latitude, msg.latitude_minutes))
-                lon = ("%02d°%07.4f'" % (msg.longitude, msg.longitude_minutes))
-                gps = "Latitude=" + str(lat) + "and Longitude=" + str(lon)
-                print(gps)
+                lat = msg.lat
+                lon = msg.lon
+                #lat = ("%02d°%07.4f'" % (msg.latitude, msg.latitude_minutes))
+                #lon = ("%02d°%07.4f'" % (msg.longitude, msg.longitude_minutes))
+                #gps = "Latitude=" + str(lat) + "and Longitude=" + str(lon)
+                #print(gps)
                 speed = msg.spd_over_grnd
                 print ('Speed over ground = ' + str(speed))
                 true_course = msg.true_course
