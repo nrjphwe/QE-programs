b5dc422608a3e86a8eb9e3f36da284d6f3b8bd01
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2021-03-10 18:48:29 +0100

    Update get_nmea1.py

--- get_nmea1.py
+++ get_nmea1.py
@@ -4,11 +4,13 @@ import sys,math
 
 # startup numbers
 lat = 19.0
-
+lon = 58.0
+wind_direction = 45
+speed = 0
 # setup db
 import mariadb
 from python_mysql_dbconfig import read_db_config
-wind_direction = 45
+
 dbconfig = read_db_config()
 conn = None
 try:
